2360105fdd83c8aea2ef2b9fd65c7cc0c12fdb25
nfiner <nfiner@umich.edu>
2020-11-02 13:11:30 -0500

    add support for the base shape being a polygon

--- cracking/sketch.js
+++ cracking/sketch.js
@@ -1,19 +1,10 @@
 let start;
-let triangles = [];
-let allTriangles = [];
+let polygons = [];
+let allPolygons = [];
 let iterations = 0;
 const MAX_ITERATIONS = 10;
 
 let centers = [];
-
-// see if point is in triangle
-function sign(p1, p2, p3) {
-    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
-}
-
-function pointIsInTriangle(pt, tri) {
-    
-}
 
 class Point {
     constructor(x, y, triangle) {
@@ -26,11 +17,6 @@ class Point {
     move() {
         this.x += 1*(Math.random() - 0.5);
         this.y += 1*(Math.random() - 0.5);
-        // if(this.triangle) {
-        //     if(!this.triangle.pointWithinTriangle(this)) {
-        //         this.rand *= -1;
-        //     }
-        // }
     }
 
     rotate(angle) {
@@ -42,51 +28,66 @@ class Point {
     }
 }
 
-class Triangle {
-    constructor(p1, p2, p3) {
-        this.p1 = p1;
-        this.p2 = p2;
-        this.p3 = p3;
-        this.color = 240 - ~~(Math.random()*50);
+class Polygon {
+    constructor(points) {
+        this.p = points;
+        // grayscale
+        // this.color = 240 - ~~(Math.random() * 50;
+
+        // dope colors
+        this.color = color(~~(Math.random()*200) + 25, ~~(Math.random()*200) + 25, ~~(Math.random()*200) + 25);
     }
 
-    pointInTriangle(randomness) {
-        let a, b, c;
-        do {
-            a = Math.random();
-            b = Math.random();
-        } while (a + b >= 1);
+    pointInPolygon(randomness) {
+        let x = 0;
+        let y = 0;
 
-        a = 1/3 + randomness*(a-1/3);
-        b = 1/3 + randomness*(b-1/3);
+        let weights = [];
+        for (let i = 0; i < this.p.length; i++) {
+            weights.push(Math.random());
+        }
+        let randWeightSum = weights.reduce((sum, w) => sum + w, 0);
+        for (let i = 0; i < this.p.length; i++) {
+            weights[i] /= randWeightSum;
+        }
 
-        c = 1 - a - b;
+        this.p.forEach((p, i) => {
+            x += (1/this.p.length*(1-randomness) + weights[i]*randomness)*p.x;
+            y += (1/this.p.length*(1-randomness) + weights[i]*randomness)*p.y;
+        });
 
-        let center_point = new Point (a*this.p1.x+b*this.p2.x+c*this.p3.x, a*this.p1.y+b*this.p2.y+c*this.p3.y, this);
+        
+        let center_point = new Point(x, y);
 
         centers.push(center_point);
 
         return center_point;
-    }
+        // let a, b, c;
+        // do {
+        //     a = Math.random();
+        //     b = Math.random();
+        // } while (a + b >= 1);
 
-    pointWithinTriangle(pt) {
-        let d1, d2, d3;
-        let has_neg, has_pos;
+        // a = 1/3 + randomness*(a-1/3);
+        // b = 1/3 + randomness*(b-1/3);
 
-        d1 = sign(pt, this.p1, this.p2);
-        d2 = sign(pt, this.p2, this.p3);
-        d3 = sign(pt, this.p3, this.p1);
+        // c = 1 - a - b;
 
-        has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-        has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+        // let center_point = new Point (a*this.p[0].x+b*this.p[1].x+c*this.p[2].x, a*this.p[0].y+b*this.p[1].y+c*this.p[2].y, this);
 
-        return !(has_neg && has_pos);
+        // centers.push(center_point);
+
+        // return center_point;
     }
 
     draw() {
         fill(this.color);
-        stroke(0, 80);
-        triangle(this.p1.x, this.p1.y, this.p2.x, this.p2.y, this.p3.x, this.p3.y);
+        stroke(0, 0);
+        beginShape();
+        this.p.forEach(p => {
+            vertex(p.x, p.y);
+        });
+        endShape(CLOSE);
     }
 }
 
@@ -97,29 +98,32 @@ function mousePressed() {
 function subdivide() {
     iterations++;
     if(iterations > MAX_ITERATIONS) return;
-    let subTriangles = [];
-    triangles.forEach(tri => {
-        if(Math.random() > 0.1) {
-            let crackCenter = tri.pointInTriangle(0.5);
-            subTriangles.push(new Triangle(tri.p1, tri.p2, crackCenter));
-            subTriangles.push(new Triangle(tri.p2, tri.p3, crackCenter));
-            subTriangles.push(new Triangle(tri.p3, tri.p1, crackCenter));
-            subTriangles.forEach(subTri => allTriangles.push(subTri));
+    let subPolygons = [];
+    polygons.forEach(poly => {
+        if(Math.random() > 0) {
+            let crackCenter = poly.pointInPolygon(1);
+            for (let i = 0; i < poly.p.length - 1; i++) {
+                subPolygons.push(new Polygon([poly.p[i], poly.p[i + 1], crackCenter]));
+            }
+            subPolygons.push(new Polygon([poly.p[poly.p.length - 1], poly.p[0], crackCenter]));
+            subPolygons.forEach(subPoly => allPolygons.push(subPoly));
         } else {
-            subTriangles.push(tri);
+            subPolygons.push(poly);
         }
     });
-    triangles = subTriangles;
+    polygons = subPolygons;
 }
 
 function create() {
-    let radius = 450;
-    let p1 = new Point(0, radius);
-    let p2 = p1.rotate(radians(120));
-    let p3 = p1.rotate(radians(240));
-    start = new Triangle(p1, p2, p3);
-    triangles.push(start);
-    allTriangles.push(start);
+    start = new Polygon([
+        new Point(400, 300),
+        new Point(500, 0),
+        new Point(400, -350),
+        new Point(-400, -200),
+        new Point(-500, 0),
+        new Point(-400, 400)]);
+    polygons.push(start);
+    allPolygons.push(start);
 }
 
 function setup() {
@@ -132,9 +136,7 @@ function draw() {
     background(15);
     translate(width/2, height/2 - 100);
     
-    centers.forEach(pt => pt.move());
+    // centers.forEach(pt => pt.move());
 
-    triangles.forEach(tri => tri.draw());
-
-    // ellipse(0, 0, 20, 20);
+    polygons.forEach(poly => poly.draw());
 }
